cba2fd8e11555a6f3599a9ea2cc405adaed9d313
Stephen Yang <YangStephen@users.noreply.github.com>
2015-10-25 21:44:05 -0400

    Update Chequing.java

--- Chequing.java
+++ Chequing.java
@@ -1,10 +1,7 @@
 /* 
-Culminating Activity
-ICS4U1
 @author Stephen Yang
 @version 1.0
 @since 2015/01/15
-Teacher: Ms. Xie
 Program Name: Chequing.java
 Description: Chequing is a class used to store the data for a Client's chequing account. This class contains the required methods to initialize, withdraw, deposit, transfer and etc. from the chequing account.
 */
